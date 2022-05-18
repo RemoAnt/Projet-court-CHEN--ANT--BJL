f13b5851708b4f7eda9e57842780c75f49abcd74
Jacky Chen <90972692+jky-chen@users.noreply.github.com>
2022-05-18 09:52:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,25 +4,25 @@
 
 PwmOut E1(P2_2);	// initialisation de E1 du moteur
 PwmOut E2(P2_3);	// initialisation de E2 du moteur
- 
+//AnalogIn D1(P0.6);    // initialisation de la diode 1 en entrée
  
 void init_GPIO()
 {
  LPC_GPIO0->FIODIR0 |= (1 << 6); //initialisation de M1 du moteur
  LPC_GPIO0->FIODIR0 |= (1 << 7); //initialisation de M2 du moteur
- LPC_GPIO0->FIOCLR0 = 1;
+ //LPC_GPIO0->FIOCLR0 = 1;
 }
 void init_E1()
 {
  E1.period(0.01);
- E1.write(0.3); //gestion de la vitesse
+ E1.write(0.5); //gestion de la vitesse
  LPC_GPIO0->FIOCLR0 = (1 << 6); //permet d'avancer
 }
 
 void init_E2()
 {
  E2.period(0.01);
- E2.write(0.3); //gestion de la vitesse
+ E2.write(0.5); //gestion de la vitesse
  LPC_GPIO0->FIOSET0 = (1 << 7); //permet de reculer
 }
 
