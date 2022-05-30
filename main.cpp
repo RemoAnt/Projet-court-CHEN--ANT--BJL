48af68171bfb1a982b8def851c9bdf29fa396aa1
Jacky Chen <90972692+jky-chen@users.noreply.github.com>
2022-05-30 09:47:05 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
 PwmOut E1(P2_2);	// initialisation de E1 du moteur
 PwmOut E2(P2_3);	// initialisation de E2 du moteur
-//AnalogIn D1(P0.6);    // initialisation de la diode 1 en entrée
- 
+AnalogIn Photo1(P0_23);    // initialisation de la diode 1 en entrée
+AnalogIn Photo2(P0_24);
+
+
 void init_GPIO()
 {
  LPC_GPIO0->FIODIR0 |= (1 << 6); //initialisation de M1 du moteur
@@ -15,21 +17,73 @@ void init_GPIO()
 void init_E1()
 {
  E1.period(0.01);
- E1.write(0.4); //gestion de la vitesse
- LPC_GPIO0->FIOCLR0 = (1 << 6); //permet d'avancer
+ E1.write(0.5); //gestion de la vitesse
+ LPC_GPIO0->FIOSET0 = (1 << 6); //permet d'avancer
 }
 
 void init_E2()
 {
  E2.period(0.01);
- E2.write(0.4); //gestion de la vitesse
+ E2.write(0.5); //gestion de la vitesse
  LPC_GPIO0->FIOSET0 = (1 << 7); //permet de reculer
 }
 
- int main (void)
+void trajectoire()
+{
+	float diff = Photo1 - Photo2;
+	if (diff<=0){
+		E1.write(0.2);
+		E2.write(0.6);
+	} else {
+		E1.write(0.6); 
+		E2.write(0.2);
+	}
+}
+
+void trajectoire2()
+{
+	float diff = Photo2 - Photo1 ; //différence de luminosité reçue entre les deux capteurs centraux
+	float valDeBase = 0.3;
+	float coeff = 10;
+	float coeff2=10;
+	if (diff<0){
+		//tourne vers capt2 (donc M1 > M2)
+		E1.write(valDeBase - diff*coeff); 
+		E2.write(valDeBase + diff*coeff);
+	} else {
+		//tourne vers capt1 (donc M1 > M2)
+		E1.write(valDeBase - diff*coeff2); 
+		E2.write(valDeBase + diff*coeff2);
+	}
+	// SI ON Y ARRIVE PAS FAIRE PAR PALIERS
+}
+
+void trajectoire3()
+{
+	float diff = Photo2 - Photo1 ; //différence de luminosité reçue entre les deux capteurs centraux
+	if (diff<0.5 and diff>-0.5){
+		E1.write(0.5); 
+		E2.write(0.5);
+	} else if(diff>0.5){
+		//tourne vers capt1 (donc M1 > M2)
+		E1.write(0.3); 
+		E2.write(0.7);
+	}
+	else
+	{
+		E1.write(0.7); 
+		E2.write(0.3);
+	}
+
+}
+	
+int main (void)
  {
 	 init_GPIO();
 	 init_E1();
 	 init_E2();
+	 while(1){
+		 trajectoire2();
+	 }
+	 
  }
-	
