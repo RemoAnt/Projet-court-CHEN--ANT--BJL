8d0cc4624b0cf8bb7ec4e0ecc2ee88c4b73726ff
Jacky Chen <90972692+jky-chen@users.noreply.github.com>
2022-05-31 09:19:00 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "mbed.h"
 
-#define PCLOCK 25e6;
 
-PwmOut E1(P2_2);	// initialisation de E1 du moteur
-PwmOut E2(P2_3);	// initialisation de E2 du moteur
+PwmOut E1(P2_2);	// initialisation de E1 du moteur droit
+PwmOut E2(P2_3);	// initialisation de E2 du moteur gauche
 AnalogIn Photo1(P0_23);    // initialisation de la diode 1 en entrée
-AnalogIn Photo2(P0_24);
+AnalogIn Photo2(P0_24);    //initialisation de la diode 2 en entrée
 
 
 void init_GPIO()
@@ -25,57 +24,60 @@ void init_E2()
 {
  E2.period(0.01);
  E2.write(0.5); //gestion de la vitesse
- LPC_GPIO0->FIOSET0 = (1 << 7); //permet de reculer
+ LPC_GPIO0->FIOSET0 = (1 << 7);
 }
 
-void trajectoire()
+void trajectoire() // fonction test du changement de direction 
 {
 	float diff = Photo1 - Photo2;
 	if (diff<=0){
-		E1.write(0.2);
-		E2.write(0.6);
-	} else {
-		E1.write(0.6); 
-		E2.write(0.2);
-	}
-}
-
-void trajectoire2()
-{
-	float diff = Photo2 - Photo1 ; //différence de luminosité reçue entre les deux capteurs centraux
-	float valDeBase = 0.3;
-	float coeff = 10;
-	float coeff2=10;
-	if (diff<0){
-		//tourne vers capt2 (donc M1 > M2)
-		E1.write(valDeBase - diff*coeff); 
-		E2.write(valDeBase + diff*coeff);
-	} else {
-		//tourne vers capt1 (donc M1 > M2)
-		E1.write(valDeBase - diff*coeff2); 
-		E2.write(valDeBase + diff*coeff2);
-	}
-	// SI ON Y ARRIVE PAS FAIRE PAR PALIERS
-}
-
-void trajectoire3()
-{
-	float diff = Photo2 - Photo1 ; //différence de luminosité reçue entre les deux capteurs centraux
-	if (diff<0.5 and diff>-0.5){
-		E1.write(0.5); 
+		E1.write(0.1);
 		E2.write(0.5);
-	} else if(diff>0.5){
-		//tourne vers capt1 (donc M1 > M2)
-		E1.write(0.3); 
-		E2.write(0.7);
+	} else {
+		E1.write(0.5); 
+		E2.write(0.1);
 	}
-	else
-	{
-		E1.write(0.7); 
-		E2.write(0.3);
-	}
-
 }
+
+void trajectoire2() //fonction qui change le vitesse de chaque roue selon la différence entre les 2 photorésistances
+{
+	float diff = Photo1 - Photo2 ; //différence de luminosité reçue entre les deux capteurs centraux
+	float valDeBase = 0.4;
+	float coeff = 60;
+	float coeff2=60;
+	if (diff<0)
+    {
+		//tourne vers capt2 (donc M1 > M2)
+		E1.write(valDeBase + diff*coeff); 
+		E2.write(valDeBase );
+	} 
+    else 
+    {
+		//tourne vers capt1 (donc M1 > M2)
+		E1.write(valDeBase + diff*coeff2); 
+		E2.write(valDeBase - diff*coeff2);
+	}
+}
+
+//void trajectoire3() //alternatif de trajectoire2 en palier
+//{
+//	float diff = Photo2 - Photo1 ; //différence de luminosité reçue entre les deux capteurs centraux
+//	if (diff<-0.5)
+//  {
+//	    E1.write(0.5); 
+//		E2.write(0.5);
+//	}
+//   else if(diff>0.5)
+//  {
+//		E1.write(0.3); 
+//		E2.write(0.7);
+//	}
+//	else
+//	{
+//		E1.write(0.7); 
+//		E2.write(0.3);
+//	}
+//}
 	
 int main (void)
  {
@@ -83,7 +85,7 @@ int main (void)
 	 init_E1();
 	 init_E2();
 	 while(1){
-		 trajectoire2();
+			trajectoire2();
 	 }
 	 
  }
